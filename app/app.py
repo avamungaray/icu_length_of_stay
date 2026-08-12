29ac3a2d7b5ae93356f61ce230cc059f78fcbe0a
Ava Mungaray <mungarayava@gmail.com>
2026-08-12 16:18:38 -0700

    Fix st.Page relative paths

--- app/app.py
+++ app/app.py
@@ -3,10 +3,10 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 pages = [
-    st.Page("app/pages/biography.py", title="Biography"),
-    st.Page("app/pages/1_resume.py", title="Resume"),
-    st.Page("app/pages/2_Projects_Overview.py", title="General Projects"),
-    st.Page("app/pages/3_ICU_Triage_Project.py", title="ICU Triage Project")
+    st.Page("pages/biography.py", title="Biography"),
+    st.Page("pages/1_resume.py", title="Resume"),
+    st.Page("pages/2_Projects_Overview.py", title="General Projects"),
+    st.Page("pages/3_ICU_Triage_Project.py", title="ICU Triage Project")
 ]
 
 pg = st.navigation(pages)
